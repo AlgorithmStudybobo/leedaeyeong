735c0ef9b2c93954c6570fe99d2b175270b08644
이대영 <kmdy7991@naver.com>
2023-08-16 02:51:29 +0900

    3th

--- 23.08.17/[PG] 표현 가능한 이진트리.java
+++ 23.08.17/[PG] 표현 가능한 이진트리.java
@@ -10,89 +10,87 @@ import java.util.*;
 
 //--------------------------- 정답 ---------------------------
 class Solution {
-    static int tree, chk;
-    static boolean[] binTree;
-    
+	static int tree, chk;
+	static boolean[] binTree;
+
 	public int[] solution(long[] numbers) {
-        int[] answer = new int[numbers.length];
-        for(int i = 0; i < answer.length; i++){
-            chk = 1;
+		int[] answer = new int[numbers.length];
+		for (int i = 0; i < answer.length; i++) {
+			chk = 1;
 			tree = 0;
-            binTree = binary(numbers[i]);
-            search(0, tree-1, false);
-            answer[i] = chk;
-        }
-        return answer;
-    }
-    
-    public static void search(int s, int e, boolean flag) {
+			binTree = binary(numbers[i]);
+			search(0, tree - 1, false);
+			answer[i] = chk;
+		}
+		return answer;
+	}
+
+	public static void search(int s, int e, boolean flag) {
 		int m = (s + e) / 2;
-		if(flag && binTree[m]) { // 루트노드 찾는데 루트노드가 0인데 자식노드가 1이면 포화 이진트리가 아님
-            chk = 0;
-            return;
-        }
-        if(s != e) {
-			search(s, m-1, !binTree[m]);
-			search(m+1, e, !binTree[m]);
+		if (flag && binTree[m]) { // 루트노드 찾는데 루트노드가 0인데 자식노드가 1이면 포화 이진트리가 아님
+			chk = 0;
+			return;
+		}
+		if (s != e) {
+			search(s, m - 1, !binTree[m]);
+			search(m + 1, e, !binTree[m]);
 		}
 	}
 
 	private static boolean[] binary(long num) {
 		int tmp = 1;
 		String b = Long.toBinaryString(num);
-		while(b.length() > tree) {
-			tree = (int) Math.pow(2, tmp++) - 1; 
+		while (b.length() > tree) {
+			tree = (int) Math.pow(2, tmp++) - 1;
 		}
 		boolean[] bin = new boolean[tree];
 		int point = tree - b.length(); // 포화 이진트리 시작지점 설정(앞에는 0으로 채움)
-        for(int i = 0; i < b.length(); i++) {
-            bin[point++] = b.charAt(i) == '1';
-        }
-		return bin; 
-	}
-}
-
-
-// ------------------------------5점--------------------------
-class Solution {
-    static int tree, chk;
-    static boolean[] binTree;
-    
-    public int[] solution(long[] numbers) {
-        int[] answer = new int[numbers.length];
-        for(int i = 0; i < answer.length; i++){
-            chk = 1;
-            binTree = binary(numbers[i]);
-            search(0, tree-1, false);
-            answer[i] = chk;
-        }
-        return answer;
-    }
-
-    private static void search(int s, int e, boolean flag) {
-		int m = (s + e) / 2;
-		if(flag && binTree[m]) { // return 0 안됨 (재귀라 뭍힘)
-            chk = 0;
-            return;
-        }
-        if(s != e) { // > 안됨(오른쪽 탐색시 s가 커짐)
-			search(s, m-1, !binTree[m]);
-			search(m+1, e, !binTree[m]);
+		for (int i = 0; i < b.length(); i++) {
+			bin[point++] = b.charAt(i) == '1';
 		}
-	}
-    
-    private static boolean[] binary(long num) { // 여기 
-		int tmp = 1;
-		String b = Long.toBinaryString(num);
-		while(b.length() > tree) {
-			tree = (int) Math.pow(2, tmp++) - 1; 
-		}
-		boolean[] bin = new boolean[tree];
-		int point = tree - b.length();
-        for(int i = 0; i < b.length(); i++) {
-            bin[point++] = b.charAt(i) == '1';
-        }
 		return bin;
 	}
 }
 
+// ------------------------------5점--------------------------
+class Solution {
+	static int tree, chk;
+	static boolean[] binTree;
+
+	public int[] solution(long[] numbers) {
+		int[] answer = new int[numbers.length];
+		for (int i = 0; i < answer.length; i++) {
+			chk = 1;
+			binTree = binary(numbers[i]);
+			search(0, tree - 1, false);
+			answer[i] = chk;
+		}
+		return answer;
+	}
+
+	private static void search(int s, int e, boolean flag) {
+		int m = (s + e) / 2;
+		if (flag && binTree[m]) { // return 0 안됨 (재귀라 뭍힘)
+			chk = 0;
+			return;
+		}
+		if (s != e) { // > 안됨(오른쪽 탐색시 s가 커짐)
+			search(s, m - 1, !binTree[m]);
+			search(m + 1, e, !binTree[m]);
+		}
+	}
+
+	private static boolean[] binary(long num) { // 여기
+		int tmp = 1;
+		String b = Long.toBinaryString(num);
+		while (b.length() > tree) {
+			tree = (int) Math.pow(2, tmp++) - 1;
+		}
+		boolean[] bin = new boolean[tree];
+		int point = tree - b.length();
+		for (int i = 0; i < b.length(); i++) {
+			bin[point++] = b.charAt(i) == '1';
+		}
+		return bin;
+	}
+}
